d8c6bf8ca885f45dfc91a0b70e682266cc0558df
checkeredtimbz <axebulamba10@gmail.com>
2025-06-23 08:15:17 -0500

    Update 6-modify_object.js

--- 6-modify_object.js
+++ 6-modify_object.js
@@ -1,4 +1,8 @@
-#!/usr/bin/node
-const myModule = require('./myVar');
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let result = "";
 
-myModule.myVar = 89;
+for (let i = 0; i < lines.length; i++) {
+  result += lines[i] + "\n"; // add a newline after each line
+}
+
+console.log(result.trim()); // .trim() removes the last extra newline
